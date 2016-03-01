011f490cc7e8b6560bbb343bd9f35bca021ccb2f
Jason Lewis <jason@decomplecting.org>
2016-03-01 14:03:04 -0500

    Fixed project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lein-ruin "0.1.0-SNAPSHOT"
-  :description "A Lein plugin to ruin things."
+  :description "A Leiningen plugin to ruin things."
   :url "https://github.com/canweriotnow/lein-ruin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
