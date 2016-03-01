fbf93d15f8dceb0b0d857004daa5193256c4a2e9
Jason Lewis <jason@decomplecting.org>
2016-03-01 13:25:43 -0500

    Works on top level files, need to recurse subdirs

--- src/leiningen/ruin.clj
+++ src/leiningen/ruin.clj
@@ -1,6 +1,21 @@
-(ns leiningen.ruin)
+(ns leiningen.ruin
+  (:require [clojire.java.io :as io]
+            [clojure.string :as str]))
+
+(defn- enum-files []
+  (let [wd (first (seq (.list (io/file "src"))))]
+    (map #(.path %)
+         (filter #(not (.isDirectory %))
+                 (seq (.listFiles (io/file (str "src/" wd))))))))
+
+(defn strip-parens [file-string]
+  (str/replace file-string "(\(|\))" ""))
 
 (defn ruin
   "I don't do a lot."
   [project & args]
-  (println "Hi!"))
+  (let [files (enum-files)]
+    (for [file files]
+      (spit file (strip-parens (slurp file))))
+    (println "RUIN!"))
+  )
